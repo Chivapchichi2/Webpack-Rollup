60af9da9af83798d6536ff1be10cfed2a144fcdc
Pavel Ivchenko <71591602+Chivapchichi2@users.noreply.github.com>
2021-08-01 01:13:11 +0300

    task4

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
-console.log("Hello World!")
+import '../index.css';
+import img from './image/Webpack.png';
+
+const webpackImg = document.createElement('img');
+webpackImg.className = 'webpackImg';
+webpackImg.src = img;
+webpackImg.alt = 'Rollup';
+
+document.body.append(webpackImg);
+
+console.log('Hello Word!');
--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,51 @@
-const path = require("path")
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  mode: "development",
-  entry: "./src/index.js",
+  mode: 'development',
+  entry: './src/index.js',
   output: {
-    filename: "build.js",
-    path: path.resolve(__dirname, "build"),
+    filename: 'build.js',
+    path: path.resolve(__dirname, 'build'),
   },
-}
+  devServer: {
+    open: true,
+    compress: true,
+    port: 4000,
+    contentBase: './build',
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'index.html',
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [['@babel/preset-env', { targets: 'defaults' }]],
+          },
+        },
+      },
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(?:ico|gif|png|jpg|jpeg)$/i,
+
+        type: 'asset/resource',
+      },
+
+      {
+        test: /\.(woff(2)?|eot|ttf|otf|svg|)$/,
+
+        type: 'asset/inline',
+      },
+    ],
+  },
+};
