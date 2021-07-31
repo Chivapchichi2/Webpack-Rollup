1c90b2726d83b79f108f70afc24612fcb16f949c
Pavel Ivchenko <71591602+Chivapchichi2@users.noreply.github.com>
2021-08-01 00:30:19 +0300

    task2

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
-console.log("Hello Word!")
+import '../index.css';
+import img from './image/Rollup.js1_.jpg';
+
+const rollupImg = document.createElement('img');
+rollupImg.className = 'rollupImg';
+rollupImg.src = img;
+rollupImg.alt = 'Rollup';
+
+document.body.append(rollupImg);
+
+console.log('Hello Word!');
